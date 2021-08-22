9e925ab4ce400707eec135ab33d60522ff4a8a3c
Riday ♡ <ridayshah2001@gmail.com>
2021-08-22 18:14:25 +0530

    ✨ Feat: 404 page
    Co-authored-by: Shivani Jha <shivaniijha99@gmail.com>

---
+++ src/pages/404.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+const NotFoundPage = () => {
+	return (
+		<div>
+			<h1>Page Not Found</h1>
+			<p>
+				Sorry, the page you are looking for doesn't exist.
+				<br />
+				Please check the URL and try again.
+			</p>
+		</div>
+	);
+};
+export default NotFoundPage;
